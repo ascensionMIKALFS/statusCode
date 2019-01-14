e4a9692ee4b099855a22e9524c69f9f511ee4447
ascensionMIKALFS <46570233+ascensionMIKALFS@users.noreply.github.com>
2019-01-14 09:10:38 -0500

    Update Ascension Status Code Picker_2.0.js

--- Ascension Status Code Picker_2.0.js
+++ Ascension Status Code Picker_2.0.js
@@ -6,10 +6,12 @@
 // @author       Pamela OConnor
 // @match        https://ascensionprod.service-now.com/*
 // @grant        Ascension
-// @downloadURL  https://raw.githubusercontent.com/ascensionMIKALFS/statusCode/master/Ascension%20Status%20Code%20Picker_2.0.js
+// @downloadURL  https://github.com/ascensionMIKALFS/statusCode/blob/master/Ascension%20Status%20Code%20Picker_2.0.js
 // @require https://code.jquery.com/jquery-3.1.1.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.js
 // ==/UserScript==
+
+// downloadURL is for updates only, does not affect script if link is broken. Also, username & pass for github account is ascensionMIKALFS / B0rg3$$!
 
 (function () {
     /**
@@ -4560,7 +4562,7 @@
                 val == 'AWVP' ||
                 val == 'CETA' ||
                 val == 'CNRC' ||
-                val == 'SCHD' 
+                val == 'SCHD'
             ) {
                 if (document.getElementById('sc_task.state')) {
                     document.getElementById('sc_task.state').value = 3;
