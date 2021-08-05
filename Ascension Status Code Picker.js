955b902a4ef9091354d1226ff07b5dbba64fbae4
ascensionMIKALFS <46570233+ascensionMIKALFS@users.noreply.github.com>
2021-08-05 16:10:05 -0400

    Update Ascension Status Code Picker.js

--- Ascension Status Code Picker.js
+++ Ascension Status Code Picker.js
@@ -6,6 +6,7 @@
 // @author       Pamela OConnor
 // @match        https://ascensionprod.service-now.com/*
 // @grant        Ascension
+// @downloadURL  https://raw.githubusercontent.com/ascensionMIKALFS/statusCode/master/Ascension%20Status%20Code%20Picker.js
 // @require https://code.jquery.com/jquery-3.1.1.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.js
 // ==/UserScript==
